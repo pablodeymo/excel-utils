10e52503720235630cee9c4900896e474d276ff8
Pablo Deymonnaz <deymonnaz@gmail.com>
2020-08-05 21:31:33 -0300

    Corrections

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ pub fn convert_date(field_value: Option<&&calamine::DataType>) -> Option<NaiveDa
         calamine::DataType::String(s) => {
             // if the fields are not separated by /, they are all together
             let v: Vec<&str> = s.split("/").collect();
-            if !v.is_empty() {
+            if v.len() == 3 {
                 let day = u32::from_str(v.get(0)?).ok()?;
                 let month = u32::from_str(v.get(1)?).ok()?;
                 let year = i32::from_str(v.get(2)?).ok()?;
                 Some(NaiveDate::from_ymd(year, month, day))
-            } else if s.len() == 8 {
+            } else if v.len() == 1 {
                 let day = u32::from_str(s.get(0..2)?).ok()?;
                 let month = u32::from_str(s.get(2..4)?).ok()?;
                 let year = i32::from_str(s.get(4..8)?).ok()?;
@@ -28,6 +28,7 @@ pub fn convert_string(field_value: Option<&&calamine::DataType>) -> Option<Strin
     match field_value {
         Some(calamine::DataType::String(s)) => Some(s.clone()),
         Some(calamine::DataType::Float(f)) => Some(format!("{}", f)),
+        Some(calamine::DataType::Int(i)) => Some(format!("{}", i)),
         _ => None,
     }
 }
@@ -35,6 +36,7 @@ pub fn convert_string(field_value: Option<&&calamine::DataType>) -> Option<Strin
 pub fn convert_i32(field_value: Option<&&calamine::DataType>) -> Option<i32> {
     match field_value {
         Some(calamine::DataType::Float(f)) => Some(*f as i32),
+        Some(calamine::DataType::Int(i)) => Some(*i as i32),
         _ => None,
     }
 }
